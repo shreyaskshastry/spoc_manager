340a6e6a559c4d27e896f73b2b96ba6bb301f471
shreyaskshastry <shreyaska1999@gmail.com>
2021-12-14 14:52:28 +0530

    lol

--- spocManager/settings.py
+++ spocManager/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zdb#^88$0-w^pijsap5y1fz#fo6n9zik0r3rvdb2bnm7**11ww'
+SECRET_KEY = 'This is a key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
